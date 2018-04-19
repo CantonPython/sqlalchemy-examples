fa6a4034439fd52f07ac12cf4f971ad19da9bb75
Michael Meffie <mmeffie@sinenomine.net>
2018-04-18 23:27:46 -0400

    example 2

--- 2/model.py
+++ 2/model.py
@@ -1,13 +1,18 @@
 
 """
-SQLAlchemy example 1
+SQLAlchemy example 2
 
 Define a table using the sqlalchemy declarative ORM.
+Insert some data to the table.
 
 """
+from __future__ import print_function
+import sys
+
 from sqlalchemy import create_engine
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
@@ -16,6 +21,22 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String, unique=True)
 
+    def __init__(self, username):
+        self.username = username
+
 
 engine = create_engine("sqlite:///example.db")
 Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+u1 = User("xyzzy")
+
+try:
+    session.add(u1)
+    session.commit()
+except Exception as e:
+    print("Failed:", e)
+    session.rollback()
+    sys.exit(1)
---
+++ 2/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+set -x
+
+python model.py
+sqlite3 example.db .schema
+sqlite3 example.db 'select * from user'
