3443b7409e720d724448731133d0e2951aac2cad
Michael Meffie <mmeffie@sinenomine.net>
2018-04-20 13:41:50 -0400

    more example queries

--- 4/test2.py
+++ 4/test2.py
@@ -4,8 +4,21 @@ from model import Session, User, Topic
 
 session = Session()
 
+# Query by name.
+print("looking for user 'xyzzy'")
 u = session.query(User).filter_by(username='xyzzy').one()
 print("username={0}, id={1}".format(u.username, u.id))
 
+# Query by id.
+print("looking for topic 1")
 t = session.query(Topic).filter_by(id=1).one()
 print("topic={0}".format(t.description))
+print("voted for topic 1")
+for user in t.voted:
+    print(user.username)
+
+# Get all.
+for user in session.query(User).all():
+    print("username={u.username}, id={u.id}".format(u=user))
+    for topic in user.topics:
+        print("  {t.votes} {t.description}".format(t=topic))
